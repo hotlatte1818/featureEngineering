edf31c2069b299d0cb096c12ea3e3cdc7d19d728
hotlatte1818 <darkSun897@protonmail.com>
2024-01-30 23:57:59 +0200

    add new styles

---
+++ LerArquivoTXT/TelaPrincipal/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TelaPrincipal
+{
+    partial class TelaImportarArquivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaImportarArquivo));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.tbCaminho = new System.Windows.Forms.TextBox();
+            this.tbSaida = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(390, 7);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.acaoBotaoBuscar);
+            // 
+            // tbCaminho
+            // 
+            this.tbCaminho.Location = new System.Drawing.Point(90, 9);
+            this.tbCaminho.Name = "tbCaminho";
+            this.tbCaminho.Size = new System.Drawing.Size(294, 20);
+            this.tbCaminho.TabIndex = 1;
+            this.tbCaminho.TextChanged += new System.EventHandler(this.tbCaminho_TextChanged);
+            // 
+            // tbSaida
+            // 
+            this.tbSaida.Location = new System.Drawing.Point(12, 36);
+            this.tbSaida.Multiline = true;
+            this.tbSaida.Name = "tbSaida";
+            this.tbSaida.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbSaida.Size = new System.Drawing.Size(470, 300);
+            this.tbSaida.TabIndex = 2;
+            this.tbSaida.WordWrap = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 342);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(470, 10);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Caminho:";
+            // 
+            // TelaImportarArquivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 364);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tbSaida);
+            this.Controls.Add(this.tbCaminho);
+            this.Controls.Add(this.btnBuscar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TelaImportarArquivo";
+            this.Text = "Importar Arquivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox tbCaminho;
+        private System.Windows.Forms.TextBox tbSaida;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
