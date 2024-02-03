8dc93115bd843dbcd18263da538351118a54e3ac
hotlatte1818 <darkSun897@protonmail.com>
2024-02-03 23:57:59 +0200

    improve performance

---
+++ LerArquivoTXT/TelaPrincipal/Banco.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TelaPrincipal
+{
+    class Banco
+    {
+        SqlConnection conexaoSQL =  null;
+        MySqlConnection conexaoMysql = null;
+
+        private string servidor;
+        private string banco;
+        private string usuario;
+        private string senha;
+
+
+        public Banco(string servidor, string banco, string usuario, string senha)
+        {
+            this.servidor = servidor;
+            this.banco = banco;
+            this.usuario = usuario;
+            this.senha = senha;
+        }
+        public void conectaSQL()
+        {
+            try
+            {
+                conexaoSQL = new SqlConnection("Data Source=" + servidor + "; Initial Catalog=" + banco + "; User id=" + usuario + "; Password=" + senha + ";");
+                conexaoSQL.Open();
+
+                MessageBox.Show("Conexão ok");
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar no banco de dados.");
+            }
+
+        }
+        public void conectaMysql(string servidor, string banco, string usuario, string senha)
+        {
+            try
+            {
+                conexaoMysql = new MySqlConnection("Data Source=" + servidor + "; Initial Catalog=" + banco + "; User id=" + usuario + "; Password=" + senha + ";");
+                conexaoMysql.Open();
+
+                MessageBox.Show("Conexão ok");
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar no banco de dados.");
+            }
+        }
+
+         
+    }
+}
